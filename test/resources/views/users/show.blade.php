c39a934330339bc7d595632e7ae3618988ed1bf7
noboni <noboni5199@gmail.com>
2018-05-05 18:20:30 +0600

    page almost done

--- test/resources/views/users/show.blade.php
+++ test/resources/views/users/show.blade.php
@@ -1,62 +1,42 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('layout')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@section('content')
+<?php
+use App\Educations;
+use App\Experiences;
+$educations = Educations::where('user_id','=',$user->id)->get();
+$experiences = Experiences::where('user_id','=',$user->id)->get();
+?>
+ @if(isset($user->avatar))
+  <img src="{{$user->avatar}}" style=" width:250px; height:300px; float:left;  margin-right:25px;">
+  @else
+    <img class="imagesize img-responsive center-block" src="{{asset('/image/default.jpg')}}" style="width:250px; height:300px; float:left; margin-right:25px;">
+  @endif
+<h1 style="text-shadow: 2px 2px 5px #00004d;" ><strong>{{ $user->name }}</strong></h1>
+<h2 style="text-shadow: 2px 2px 5px #00004d; font-size: 20px; padding-left: 250px;">Profession:{{ $user->profession }}</h2>
+@foreach($experiences as $experience)
+@if($experience->present)
+  <h2 style="text-shadow: 2px 2px 5px #00004d; width: 80%; padding-left: 500px;font-size: 20px;">{{$experience->designation}} at {{$experience->work_place}}</h2>
+@else
+  <h2 style="text-shadow: 2px 2px 5px #00004d; width: 80%; padding-left: 500px;font-size: 15px;">Worked as {{$experience->designation}} at {{$experience->work_place}}</h2>
+@endif
+@endforeach
+<br>
+@foreach($educations as $education)
+@if($education->present)
+  <h2 style="text-shadow: 2px 2px 5px #00004d; width: 80%; padding-left: 500px;font-size: 20px;">Studies {{$education->degree}} at {{$education->department}},{{ $education->institution }}</h2>
+@else
+  <h2 style="text-shadow: 2px 2px 5px #00004d; width: 80%; padding-left: 500px;font-size: 15px;">Studied {{$education->degree}} at {{$education->department}},{{ $education->institution }}</h2>
+@endif
+@endforeach
 
-    <title>Appointment Assistant</title>
-  </head>
-  <body>
-    <header>
-      <div class="collapse bg-dark" id="navbarHeader">
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-8 col-md-7 py-4">
-              <h4 class="text-white">About</h4>
-              <p class="text-muted">Appointment Assistant lets you make and manage your appointments from anywhere.</p>
-            </div>
-            <div class="col-sm-4 offset-md-1 py-4">
-              <h4 class="text-white">Contact</h4>
-              <ul class="list-unstyled">
-                <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                <li><a href="#" class="text-white">Like on Facebook</a></li>
-                <li><a href="#" class="text-white">Email me</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="navbar navbar-dark bg-dark box-shadow">
-        <div class="container d-flex justify-content-between">
-          <a href="/" class="navbar-brand d-flex align-items-center">
-            
-            <strong>Appointment Assistant</strong>
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-        </div>
-      </div>
-    </header>
-
-    <main role="main">
-
-      <section class="jumbotron text-center">
-        <div class="container">
-              <div class="center-block">
-                <h1>{{ $user->name }}</h1>
-<h2>Profession: {{ $user->profession }}</h2>
-<h2>Phone: {{ $user->phone }}</h2>
-<h2>Email: {{ $user->email }}</h2>
-<h2>Rating: {{ $user->rating }}</h2>
+<h2 style="text-shadow: 2px 2px 5px #00004d; padding-left: 200px;font-size: 20px;">Phone:{{ $user->phone }}</h2>
+<h2 style="text-shadow: 2px 2px 5px #00004d; padding-left: 200px; font-size: 20px;">Email:{{ $user->email }}</h2>
+<h2 style="text-shadow: 2px 2px 5px #00004d; padding-left: 250px; font-size: 20px;">Rating:{{ $user->rating }}</h2>
 <br>
 <br>
 <br>
-<form id="demo" class="rating" method="POST" action="/setrating/{{$user->id}}">
+<form id="demo" class="rating" method="POST" action="/setrating/{{$user->id}}" style="padding-left: 250px;">
   {{ csrf_field() }}
   <input type="submit" style="position: absolute; left: -9999px"/>
   <label>
@@ -91,7 +71,7 @@
   </label>
 
 </form>
-<form method="GET" action="/placeappointments/{{$user->id}}">
+<form method="GET" action="/placeappointments/{{$user->id}}" style="padding-left: 250px;">
       {{ csrf_field() }}
       <div class="top-right links">
             <button type="submit" class="btn btn-primary">Place Appointment</button>
@@ -164,33 +144,7 @@
   text-shadow: 0 0 5px #09f;
 }
 </style>
-              </div>
-        </div>
-      </section>
-
-    <footer class="text-muted">
-      <div class="container">
-        <p class="float-right">
-          <a href="#">Back to top</a>
-        </p>
-        <p>New to Appointment Assistant? <a href="/">Visit the homepage</a> or read our <a href="../../getting-started/">getting started guide</a>.</p>
-      </div>
-    </footer>
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-  <script type="text/javascript">
-  $(document).ready(function() { 
-     $('input[type=radio]').change(function(){
-          $('input[type=submit]').trigger('click');
-          });
-     });
-</script>
-</html>
+@endsection
 
 
 
